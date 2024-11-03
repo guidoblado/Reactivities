15a32bcde30578eccdd50d76a49f5b210e883980
Guido Caballero <guidoblado@gmail.com>
2024-11-03 16:27:01 -0400

    End of section 5

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './app/layouts/styles.css'
 import 'semantic-ui-css/semantic.min.css'
-import App from './App.tsx'
+import App from './app/layouts/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
