9b5fc9dddb40af01e436c511f8bde459794b0103
Guido Caballero <guidoblado@gmail.com>
2024-11-04 19:36:28 -0400

    End of section 7

--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './app/layouts/styles.css'
 import 'semantic-ui-css/semantic.min.css'
 import App from './app/layouts/App'
+import { store, StoreContext } from './app/stores/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </StrictMode>,
 )
